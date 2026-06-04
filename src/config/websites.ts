a9e8f489748b65e10bbcc048c4837aa87af305b1
Lord shadow <rkive991@gmail.com>
2026-06-04 16:27:32 +0600

    Add src/config/websites.ts

---
+++ src/config/websites.ts
@@ -0,0 +1,31 @@
+type WebsiteConfig = {
+  BASE: string;
+};
+
+export type AnimeWebsiteConfig = WebsiteConfig & {
+  CLONES?: Record<string, string[]>;
+};
+
+type Websites = Record<string, AnimeWebsiteConfig>;
+
+// anime websites and their clones
+export const websites_collection: Websites = {
+  AniWatch: {
+    BASE: "https://aniwatchtv.to",
+    CLONES: {
+      HiAnime: [
+        "https://hianimez.is",
+        "https://hianimez.to",
+        "https://hianime.nz",
+        "https://hianime.bz",
+        "https://hianime.pe",
+      ],
+    },
+  },
+  GogoAnime: {
+    BASE: "https://ww24.gogoanimes.fi",
+  },
+  KickAssAnime: {
+    BASE: "https://kickass-anime.ro",
+  },
+};
